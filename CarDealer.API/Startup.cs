ff9cd18ca7a5e72c58e77c2fc8e60179ee9af6af
SLTC-GroupProject <57896573+SLTC-GroupProject@users.noreply.github.com>
2019-12-05 22:16:04 +0530

    added data part

---
+++ CarDealer.API/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarDealers.DataManager.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CarDealer.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddDbContext<CarDealersDbContext>();
+            string connectionString = Configuration["connectionStrings:cityDbConnectionString"];
+            services.AddDbContext<CarDealersDbContext>(o => o.UseSqlServer(connectionString));
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ CarDealers.DataManager/Context/CarDealersDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealers.DataManager.Context
+{
+    public class CarDealersDbContext : DbContext
+    {
+        public CarDealersDbContext(DbContextOptions<CarDealersDbContext> options) :base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        //{
+        //    string sqlConnectionString = "Server=DESKTOP-7UFK8SA\\SQL2016; Database=CarDealersDb; Trusted_Connection=True";
+        //    string sqlConnectionString = "Server=DESKTOP-7UFK8SA\\SQL2016; Database=CarDealersDb;Integrated Security=SSPI; User ID = sa ;Password=welcome@123;
+        //    builder.UseSqlServer(sqlConnectionString);
+        //    base.OnConfiguring(builder);
+        //}
+    }
+}
+    
